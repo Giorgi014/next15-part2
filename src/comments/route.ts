089b41bd8e7cd67bb09e4ec652d4b2efc09362dc
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-18 18:02:55 +0400

    project update

--- src/comments/route.ts
+++ src/comments/route.ts
@@ -3,3 +3,16 @@ import { comments } from "./data";
 export async function GET() {
   return await Response.json(comments);
 }
+
+export async function POST(requst: Request) {
+  const comment = await requst.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
+}
