8094366da7643968ca462a4086120fa537edffb9
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-19 22:39:54 +0400

    project update

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,16 +1,25 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  // const requestHeaders = new Headers(request.headers);
-  // console.log(requestHeaders.get("Authorization"));
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get("Authorization"));
 
   const headersList = await headers();
   console.log(headersList.get("Authorization"));
 
-  return new Response("<h1>Profile API Data<h1/>", {
+  const theme = request.cookies.get("theme");
+  console.log(theme);
+
+  const coockieStore = await cookies();
+  coockieStore.set("resultsPerPage", "20");
+  console.log(coockieStore.get("resultsPerPage"));
+  
+
+  return new Response("<h1>Profile API Data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
