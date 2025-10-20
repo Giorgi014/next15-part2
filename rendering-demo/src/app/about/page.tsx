a1a89a25610c1369da4d90b7da6de7fb1b4b1099
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-20 23:56:21 +0400

    project update

---
+++ rendering-demo/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { useState } from "react";
+
+export default function AboutPage() {
+  const [name, setName] = useState("");
+  console.log("About server component");
+  return <h1>About page</h1>;
+}
