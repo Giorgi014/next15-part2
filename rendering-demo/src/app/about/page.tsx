efef69fd2d6c22f8d03defb6b47f509444fb427d
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-21 23:45:10 +0400

    project update

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,8 +1,9 @@
-"use client";
-import { useState } from "react";
+import { cookies } from "next/headers";
 
-export default function AboutPage() {
-  const [name, setName] = useState("");
+export default async function AboutPage() {
+  const cookiesStore = await cookies();
+  const theme = cookiesStore.get("theme");
+  console.log(theme);
   console.log("About server component");
   return <h1>About page {new Date().toLocaleTimeString()}</h1>;
 }
