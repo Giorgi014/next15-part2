123827fcbb46b0c6ba7159b29de4ee908e212098
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-23 01:29:14 +0400

    project update

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -5,13 +5,20 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 // import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ClientRoutePage() {
   // const result = serverSideFunction();
   const theme = useTheme();
+  const result = clientSideFunction();
   const settings = {
     dots: true,
   };
-  return <h1 style={{ color: theme.colors.secondary }}>Client router page</h1>;
+  return (
+    <>
+      <h1 style={{ color: theme.colors.secondary }}>Client router page</h1>
+      <p>{result}</p>
+    </>
+  );
 }
