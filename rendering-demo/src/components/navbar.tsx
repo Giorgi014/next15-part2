2263aa998177ade9660b28f43eab192a47f11632
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-10-24 01:09:23 +0400

    project update

---
+++ rendering-demo/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { NavLinks } from "./nav-links";
+import { NavSearch } from "./nav-search";
+
+export const Navbar = () => {
+  console.log(`Navbar rendered`);
+  return (
+    <div>
+      <NavLinks />
+      <NavSearch />
+    </div>
+  );
+};
